5b5574ee99e81815bd3563a611323a7472029de4
SHAIK-MOHAMMAD-IRFAN27 <shaikmohammadirfan@273@gmail.com>
2025-04-17 17:15:51 +0530

    Merge remote changes

--- signlang.py
+++ signlang.py
@@ -68,6 +68,3 @@ async def predict(file: UploadFile = File(...)):
         "prediction_letter": predicted_char
     }
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
